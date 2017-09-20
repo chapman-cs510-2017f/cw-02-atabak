4509b5b81b20d603748a2bc1c9da30767217683e
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-19 17:01:31 -0700

    Instructor comments

--- createfiles.sh
+++ createfiles.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 mkdir tmpfiles
-cd tmpfiles
+cd tmpfiles                 ### It is polite to return to the original directory
 touch file{001..100}.tmp
 for i  in {001..100}; do
-echo  "Temporary file $i" >> file$i.tmp;
+echo  "Temporary file $i" >> file$i.tmp;  ### It is convention to indent when inside for and if blocks
 done
+
+### cd - ### This returns to the original directory
